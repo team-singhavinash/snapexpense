b554cc63d59f81a83ad88fc0e860dfe7c68b0e0b
avinash singh <avinashsingh@winjit.com>
2020-02-20 12:20:22 +0530

    Camera Store i.e Controller created

---
+++ lib/controller/customcamera_store.dart
@@ -0,0 +1,8 @@
+import 'package:mobx/mobx.dart';
+part 'customcamera_store.g.dart';
+
+class CustomCameraStore = _CustomCameraStore with _$CustomCameraStore;
+
+abstract class _CustomCameraStore with Store{
+
+}
