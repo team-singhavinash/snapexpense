b41ab28c7793b4db1f40f034e0ca1467f88de8f0
avinash singh <avinashsingh@winjit.com>
2020-02-25 00:17:13 +0530

    added

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Snapexpenses',
+      title: 'Snap Expense',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
